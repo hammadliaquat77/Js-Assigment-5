099395e487d6002359a3932a5552916726864839
hammadprogrammer77 <154601362+hammadprogrammer77@users.noreply.github.com>
2024-05-10 22:09:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,186 @@
+
+// question 1 ? plus
+
+var plus1 = "plus consept"
+console.log(plus1);
+
+
+
+var num = 3
+var num1 = 5
+
+
+console.log(num + "=====> num +");
+console.log(num1 + "====> num1 +");
+console.log(num + num1);
+
+
+// question 2 ? multiplication
+
+var multiply = "multiply consept"
+console.log(multiply);
+
+var num = 3
+var num1 = 5
+
+
+console.log(num + "=====> num *");
+console.log(num1 + "====> num1 *");
+console.log(num * num1);
+
+
+// division
+
+var devide = "devide consept"
+console.log(devide);
+
+
+var num = 3
+var num1 = 5
+
+
+console.log(num + "=====> num /");
+console.log(num1 + "====> num1 /");
+console.log(num / num1);
+
+
+// modulus
+
+var modulus = "modulus consept"
+console.log(modulus);
+
+
+var num = 3
+var num1 = 5
+
+
+console.log(num + "=====> num %");
+console.log(num1 + "====> num1 %");
+console.log(num % num1);
+
+
+// minus --
+
+var minus = "minus consept"
+console.log(minus);
+
+
+var num = 3
+var num1 = 5
+
+
+console.log(num + "=====> num -");
+console.log(num1 + "====> num1 -");
+console.log(num - num1);
+
+
+// question 3 ? reminder 0
+
+var reminder = "reminder 0"
+console.log(reminder);
+
+
+var hello = "5"
+var hello1 = "6"
+var hello2 = "13"
+var hello3 = "12"
+
+console.log(hello);
+console.log(hello1);
+console.log(hello2);
+console.log(hello3);
+console.log(hello / hello1 / hello2 / hello3);
+
+
+// question 4 ?
+
+var ticket = "1 ticket cost of 600 pkr and five ticket cost how much?"
+console.log(ticket);
+
+var ticket1 = 600
+console.log(ticket1 + "====> plus");
+
+var ticket1 = 600
+console.log(ticket1 + "====> plus");
+
+var ticket1 = 600
+console.log(ticket1 + "====> plus");
+
+var ticket1 = 600
+console.log(ticket1 + "====> plus");
+
+var ticket1 = 600
+console.log(ticket1 + "====> plus");
+
+
+var ticket2 = "3000 pkr"
+console.log(ticket2);
+
+var ticket5 = "five ticket cost of 3000 pkr rupees"
+console.log(ticket5);
+
+
+
+
+// question 5 ? table of 4 
+
+var table = prompt("enter of table number")
+var define = document.write( table + "x 1=" + table * 1 + "</br>");
+document.write(table + "x 2=" + table * 2 + "</br>");
+document.write(table + "x 3=" + table * 3 + "</br>");
+document.write(table + "x 4=" + table * 4 + "</br>");
+document.write(table + "x 5=" + table * 5 + "</br>");
+document.write(table + "x 6=" + table * 6 + "</br>");
+document.write(table + "x 7=" + table * 7 + "</br>");
+document.write(table + "x 8=" + table * 8 + "</br>");
+document.write(table + "x 9=" + table * 9 + "</br>");
+document.write(table + "x 10=" + table * 10);
+
+
+//Question 7 ecomerce store
+
+var shopping="shopping cart"
+document.write("<h1>" + shopping + "</h1>")
+
+
+var price1 ="price of item 1 is 650 " + "</br>"
+var quantity= "quantity of item 1 or 3 " + "</br>"
+
+document.write(price1 + quantity); 
+
+
+
+var price2 ="price of item 2 is 100 " + "</br>"
+var quantity2= "quantity of item 2 is 7 " + "</br>" + "</br>"
+document.write(price2 + quantity2);
+
+
+
+var shipping ="Shipping charges Rs.100 " + "</br>" + "</br>"
+document.write(shipping);
+
+
+var toalCost= "Total Cost of your order is Rs.2750"
+document.write(toalCost);
+
+
+
+// question 8 
+
+var mark = "MarkSheet"
+document.write("<h1>" + mark + "</h1>")
+
+
+var student1 = "Total Marks 550" + "</br>"
+var student2 = "Marks Obtained 487" + "</br>" + "</br>"
+var percentage= "Percentage: 88.54545454"
+
+document.write(student1); 
+document.write(student2);
+document.write(percentage)
+
+
+
+
+
+
